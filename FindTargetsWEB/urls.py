c024063cdd8ea97db74a5b868ee4e246935dda73
fael-silva <rafaelvicent7@gmail.com>
2022-04-07 09:51:57 -0300

    Fifth  commit

---
+++ FindTargetsWEB/urls.py
@@ -0,0 +1,15 @@
+#from django.conf.urls import url
+#from views import index, passo1, passo2, download
+from . import views
+from django.urls import re_path as url
+from django.conf import settings
+from django.conf.urls.static import static
+
+app_name = "FindTargetsWEB"
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'passo1/$', views.passo1, name='passo1'),
+    url(r'passo2/$', views.passo2, name='passo2'),
+    url(r'download/$', views.download, name='download'),
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
